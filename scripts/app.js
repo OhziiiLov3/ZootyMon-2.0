067f0e1b6006c3c933e7b7af6d17a31f8a71fac4
ohziiilov3 <klbaskerville0520@gmail.com>
2022-07-30 11:11:03 -0700

    added velocity to push square down the window browser, also added simple style to rect

--- scripts/app.js
+++ scripts/app.js
@@ -16,14 +16,33 @@ class Player{
             x: 100,
             y: 100,
         }
+        this.velocity ={ // pushes player and down 
+            x: 0,
+            y: 1, 
+        }
         this.width = 50;
         this.height = 50;
     }
     draw(){
         // .fillRect() method draws a fill rectangle w/ starting point x,y 
+        context.fillStyle = 'purple'
         context.fillRect(this.position.x, this.position.y, this.width , this.height)
+    }
+
+    update(){
+        // alter player properties 
+        this.draw();
+        this.position.y += this.velocity.y;
     }
 }
 
 const player = new Player();
 player.draw()
+
+function animate(){
+    requestAnimationFrame(animate);
+    context.clearRect(0,0, canvas.width, canvas.height)
+    player.update();
+}
+
+animate();
